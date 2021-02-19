bcd7c296f6110e998cff9280e4f41b2b2eb4a628
federico29 <federico.barrios@mail.escuelaing.edu.co>
2021-02-19 16:08:31 -0500

    Parcial arep

--- src/main/java/edu/escuelaing/arep/App.java
+++ src/main/java/edu/escuelaing/arep/App.java
@@ -1,13 +1,39 @@
 package edu.escuelaing.arep;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import static spark.Spark.*;
+
+import spark.Request;
+import spark.Response;
+
+public class App {
+    
+    public static void main( String[] args ){
+        port(getPort());
+        get("/hello", (req, res) -> 
+                "<!DOCTYPE html>"
+                + "<html>"
+                + "<body>"
+                + "<h1>Calculadora Trigonometrica</h1>"
+                + "<p> Ingrese la funcion trigonometrica y el valor: <br> " 
+                + "<form action=\"/respuesta\">"
+                + "  Función:<br>"
+                + "  <input type=\"text\" name=\"data\" >"
+                + "  <br>"
+                + "  Valor:<br>"
+                + "  <input type=\"text\" value=\"value\">"
+                + "  <br>"
+                + "  <br>"
+                + "  <input type=\"submit\" value=\"Calcular\">"
+                + "</form>"
+                + "</body>"
+                + "</html>");
+        notFound("<html><body><h1>Error 404: Página no encontrada</h1></body></html>");
+    }
+    
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set(i.e. on localhost)
     }
 }
